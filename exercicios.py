ec4b4bd39e2b0a1ce7b830c733351bd3f456556d
Jesse <jesseabe.rodrigues@gmail.com>
2024-06-11 22:01:54 -0300

    adicionando try except

--- exercicios.py
+++ exercicios.py
@@ -1,12 +1,13 @@
 import math
 
 #01 - Faça um programa que pe;a dois números inteiros e imprima a divisão inteira do primeiro pelo segundo
-numero_01 = int(input("Inserir um numero inteiro: "))
-numero_02 = int(input("Inserir outro numero inteiro: "))
-
-resultado = numero_01 // numero_02
-print(resultado)
-
+try:
+    numero_01 = int(input("Inserir um numero inteiro: "))
+    numero_02 = int(input("Inserir outro numero inteiro: "))
+    resultado = numero_01 // numero_02
+    print(resultado)
+except ZeroDivisionError as e:
+    print(e)
 
 #02 - Escreva um programa que calcule a area do círculo recebendo o raio como entrada
 raio_do_circulo = float(input("Digite o raio: "))
